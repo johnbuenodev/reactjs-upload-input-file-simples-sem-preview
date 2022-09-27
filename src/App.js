d0fb092ae4d4dd7cb87575774131dc5b31109b14
johnbuenodev <johnbuenodev@gmail.com>
2022-09-27 19:25:54 -0300

    exemplo de upload file simples com input file(sem preview), chamada para enviar registro para o backend(precisa de endpoint funcionando)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+class App extends Component {
+
+  state = {
+   selectedFile: null
+  }
+
+  fileSelectedHandler = (event) => {
+  
+   const file = event.target.files[0]; 
+
+   if (file && !file.name) {
+      window.alert("Selecione um arquivos de imagem!");
+      return false;
+   }
+   if (file.size > 10e6) { //10mb
+     window.alert("Escolha um arquivo com menos de 10 MB!");
+     return false;
+   }
+
+   console.log(file); //C:\fakepath\cabelo1.jpg
+   console.log(file.name); //cabelo1.jpg
+   console.log(file.size); //47897
+
+    if(file &&file.size < 10e6) { 
+     this.setState({
+      selectedFile: file//event.target.files[0]
+     })
+     file = null;
+    }
+   /*
+
+   lastModified: 1639668951678
+   lastModifiedDate: Thu Dec 16 2021 12:35:51 GMT-0300 (Horário Padrão de Brasília)
+   name: "CaseAndMolly.jpg"
+   size: 114718
+   type: "image/jpeg"
+   webkitRelativePath:  ""
+
+  */
+  }
+
+  fileUploadHandler = async (event) => {
+    
+    if(this.state.selectedFile) {
+       //criando objeto
+       const fd = new FormData();
+       fd.append('image', this.state.selectedFile, this.state.selectedFile.name) 
+
+       //mandando para nuvem exe endpoint precisa de backend vai estourar erro
+       await axios.post('http://endereco-nuvem.com/images', fd, {
+        onUploadProgress: (data) => {
+          console.log(data.loaded," / " ,data.total, "%");
+          console.log(Math.round(data.loaded / data.total * 100) + "%");
+        }
+       })
+         .then( res => {  
+          console.log(res);
+          this.setState({
+            selectedFile: null
+           })
+         })
+         .catch( error => {
+          console.log(error); 
+          this.setState({
+            selectedFile: null
+           })
+         });
+
+        }
+   
+  }
+  
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input type="file" onChange={this.fileSelectedHandler}/>
+     { this.state.selectedFile ? <button onClick={this.fileUploadHandler}>UPLOAD FILE</button> : null}
     </div>
-  );
+   )
+  }
 }
 
 export default App;
